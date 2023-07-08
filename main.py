bbb3985c040921eb0cae30e839ee860ab4fd6fbc
AlekseiKalganov <kalganoff.aleksei@gmail.com>
2023-07-08 10:45:08 +0300

    feat: добавил кнопки

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import telebot
-
+import webbrowser
+from telebot import types
 bot = telebot.TeleBot('6383848728:AAExXNVJkqYT5UfDJyvnHLr1x8hFP3y_GN0')
 
+@bot.message_handler(commands=['site', 'website'])
+def site(message):
+    webbrowser.open('https://mail.ru')
+
+@bot.message_handler(content_types='photo')
+def get_photo(message):
+    markup = types.InlineKeyboardMarkup()
+    btn1 = types.InlineKeyboardButton('Перейти на сайт', url='https://google.com')
+    markup.add(btn1)
+    btn2 = types.InlineKeyboardButton('Удалить фото', callback_data='delete')
+    btn3 = types.InlineKeyboardButton('Изменить текст', callback_data='edit')
+    markup.row(btn2, btn3)
+    bot.reply_to(message, 'Какое красивое фото!', reply_markup=markup)
 
 @bot.message_handler(commands=['start'])
 
